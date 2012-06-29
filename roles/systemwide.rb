0cd13579d43d9ba6ea93637df7c492da3092ca93
Ben Browning <bbrownin@redhat.com>
2012-06-29 10:33:40 -0400

    Revert "Use OpenJDK instead of Oracle Java"
    This reverts commit cb41eaa13d449a9e3dc35241d8bb958628de7ee7.

--- roles/systemwide.rb
+++ roles/systemwide.rb
@@ -1,17 +1,1 @@
-name        'systemwide'
-description 'top level attributes, applies to all nodes'
-
-run_list *%w[
-  build-essential
-  motd
-  zsh
-  emacs
-  ntp
-  ]
-
-default_attributes({
-  :java        => { # use openjdk
-    :install_flavor => 'openjdk',
-    :jdk_version => 6
-  },
-})
+../vendor/ironfan-pantry/roles/systemwide.rb
