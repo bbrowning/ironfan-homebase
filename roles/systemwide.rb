cb41eaa13d449a9e3dc35241d8bb958628de7ee7
Ben Browning <bbrownin@redhat.com>
2012-06-29 09:55:28 -0400

    Use OpenJDK instead of Oracle Java

--- roles/systemwide.rb
+++ roles/systemwide.rb
@@ -1,1 +1,17 @@
-../vendor/ironfan-pantry/roles/systemwide.rb
+name        'systemwide'
+description 'top level attributes, applies to all nodes'
+
+run_list *%w[
+  build-essential
+  motd
+  zsh
+  emacs
+  ntp
+  ]
+
+default_attributes({
+  :java        => { # use openjdk
+    :install_flavor => 'openjdk',
+    :jdk_version => 6
+  },
+})
